734c4a35aed536e57e987ca37f17934ecbbff985
FireStreaker2 <michaelccc2019@outlook.com>
2024-08-08 09:36:26 -0700

    gold border

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,24 +38,28 @@ export default function Home() {
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen w-full flex-col items-center px-4 py-12 md:px-64">
-        <div className="flex w-full flex-col justify-between md:flex-row">
-          <div className="flex flex-col text-left">
-            <h1 className="text-5xl text-yellow-500">{data.meta.main.name}</h1>
-            <p className="text-3xl">{data.meta.main.title}</p>
-          </div>
-          <div className="mt-4 flex flex-col text-left md:mt-0 md:text-right">
-            {Object.keys(typedData.meta.other).map((item) => (
-              <h1 key={item}>
-                {item.charAt(0).toUpperCase() + item.slice(1)}:{" "}
-                {typedData.meta.other[item]}
+        <div className="border-2 border-yellow-400 p-2 sm:border-4 sm:p-6 md:p-10">
+          <div className="flex w-full flex-col justify-between md:flex-row">
+            <div className="flex flex-col text-left">
+              <h1 className="text-5xl text-yellow-500">
+                {data.meta.main.name}
               </h1>
+              <p className="text-3xl">{data.meta.main.title}</p>
+            </div>
+            <div className="mt-4 flex flex-col text-left md:mt-0 md:text-right">
+              {Object.keys(typedData.meta.other).map((item) => (
+                <h1 key={item}>
+                  {item.charAt(0).toUpperCase() + item.slice(1)}:{" "}
+                  {typedData.meta.other[item]}
+                </h1>
+              ))}
+            </div>
+          </div>
+          <div className="flex w-full flex-col">
+            {Object.keys(typedData.other).map((item) => (
+              <Section title={item} data={typedData.other[item]} key={item} />
             ))}
           </div>
-        </div>
-        <div className="flex w-full flex-col">
-          {Object.keys(typedData.other).map((item) => (
-            <Section title={item} data={typedData.other[item]} key={item} />
-          ))}
         </div>
       </main>
     </>
