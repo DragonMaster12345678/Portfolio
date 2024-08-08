91c01305a1f3bd67c8873cf4fb81b3f6f08384e8
FireStreaker2 <michaelccc2019@outlook.com>
2024-08-08 10:42:02 -0700

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <GoogleTagManager gtmId="GTM-5ZCD8X9L" />
+      <GoogleAnalytics gaId="G-CPC16CX538" />
     </>
   );
 }
